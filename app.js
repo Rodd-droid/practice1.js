0b6daec4aa07d3b66480afdfc5048a453ef58d04
Rodd-droid <rodd_brian@outlook.com>
2024-11-26 19:21:03 -0500

    feat: :sparkles: Update proyect

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.json({ message: "Hello, World from Dockerized Express App!" });
-});
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); 
 
-app.get('/greet/:name', (req, res) => {
-    const name = req.params.name;
-    res.json({ message: `Hello, ${name}!` });
+app.get('/', (req, res) => {
+    res.render('index', { message1: "When people make fun of my code", message2: "but I'm just a Child Guy" });
 });
 
 app.listen(PORT, () => {
